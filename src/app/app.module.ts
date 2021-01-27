3307babc3511e099e79c65610cf50fbbba36a728
Matheus Rian <souzamatheusrian@gmail.com>
2021-01-27 09:52:30 -0200

    feat: read Product

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -26,6 +26,11 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductRead2Component } from './components/product/product-read2/product-read2.component';
+import { MatTableModule } from '@angular/material/table';
+import { MatPaginatorModule } from '@angular/material/paginator';
+import { MatSortModule } from '@angular/material/sort';
 @NgModule({
   declarations: [
     AppComponent,
@@ -36,7 +41,9 @@ import { MatInputModule } from '@angular/material/input';
     ProductCrudComponent,
     RedDirective,
     ForDirective,
-    ProductCreateComponent
+    ProductCreateComponent,
+    ProductReadComponent,
+    ProductRead2Component
   ],
   imports: [
     HttpClientModule,
@@ -51,7 +58,10 @@ import { MatInputModule } from '@angular/material/input';
     AppRoutingModule,
     FormsModule,
     MatFormFieldModule,
-    MatInputModule
+    MatInputModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/product/product-read/product-read.component.ts
@@ -0,0 +1,28 @@
+
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../product.model';
+import { ProductService } from '../product.service';
+
+@Component({
+  selector: 'crud-product-read',
+  templateUrl: './product-read.component.html',
+  styleUrls: ['./product-read.component.css']
+})
+export class ProductReadComponent implements OnInit {
+
+  products: Product[]
+
+  constructor(private productService: ProductService ) { }
+
+  ngOnInit(): void {
+    this.read();
+  }
+
+  read(): void {
+    this.productService.read()
+      .subscribe(products => {
+        this.products = products
+        console.log(products)
+      });
+  }
+}
